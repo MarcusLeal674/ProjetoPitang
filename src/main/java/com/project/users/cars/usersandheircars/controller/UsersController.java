1e61395a115ffe0365ff6e8a7ec73d6d1df7bf23
MARCUS ANTONIUS LEAL DE ANDRADE <marcusleal674@gmail.com>
2024-02-23 10:57:13 -0300

    Controller

--- src/main/java/com/project/users/cars/usersandheircars/controller/UsersController.java
+++ src/main/java/com/project/users/cars/usersandheircars/controller/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.project.users.cars.usersandheircars.utils.UserCarUtility;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api")
 public class UsersController {
